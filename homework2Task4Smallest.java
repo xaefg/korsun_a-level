94783272b92735538cb238b145854bbc530ddfac
Roman <md.zoidberg.3000@gmail.com>
2022-09-06 14:01:52 +0300

    homework2

--- homework2Task4Smallest.java
+++ homework2Task4Smallest.java
@@ -11,13 +11,15 @@ public class homework2Task4Smallest {
     }
 
     public static String smallest(int num1, int num2, int num3) {
+
         /*boolean a = Math.abs(num1)<Math.abs(num2)&&Math.abs(num1)<Math.abs(num3)&&Math.abs(num1)!=Math.abs(num2)&&Math.abs(num1)!= Math.abs(num3);
         boolean b = Math.abs(num2)<Math.abs(num1)&&Math.abs(num2)<Math.abs(num3)&&Math.abs(num2)!=Math.abs(num1)&&Math.abs(num2)!= Math.abs(num3);
         boolean c = Math.abs(num3)<Math.abs(num1)&&Math.abs(num3)<Math.abs(num2)&&Math.abs(num3)!=Math.abs(num1)&&Math.abs(num3)!= Math.abs(num2);
         return a ? (num1+" the smallest") : (b ? (num2+" the smallest") : (c ? (num3+" the smallest") : "You've entered same numbers"));*/
+
         int a = num1 > 0 ? num1 : -num1;
         int b = num2 > 0 ? num2 : -num2;
         int c = num3 > 0 ? num3 : -num3;
-        return a>b&&a>c ? (num1+" the smallest") : (b>a&&b>c? (num2+" the smallest") : (c>a&&c>b ? (num3+" the smallest") : "You've entered same numbers"));
+        return a<b&&a<c ? (num1+" the smallest") : (b<a&&b<c? (num2+" the smallest") : (c<a&&c<b ? (num3+" the smallest") : "You've entered same numbers"));
     }
 }
