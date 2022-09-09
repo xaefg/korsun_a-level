7241755714d587271c8e14f65597aca37be63f64
Roman <md.zoidberg.3000@gmail.com>
2022-09-09 18:31:37 +0300

    practice 3_5

---
+++ src/ua/korsun/practice3/Task_3_5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task_3_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int a = sc.nextInt();
+        System.out.println("Enter exponent");
+        int b = sc.nextInt();
+        System.out.println(power(a, b));
+    }
+
+    public static int power(int a, int b) {
+        int result = 1;
+        if (b == 0) {
+            return result;
+        } else {
+            while (b != 0) {
+                result *= a;
+                --b;
+            }
+        }
+        return result;
+    }
+}
