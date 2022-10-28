4283be479e7eada3af4db24cafe6eedf65ab0e3f
Roman <md.zoidberg.3000@gmail.com>
2022-10-28 10:50:29 +0300

    fix

--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -8,11 +8,11 @@ public class Task2Test {
 
     @Test
     void checker_Sort_False() {
-        int[][] array = new int[][]{{10, 402, 3}, {123, 120, 110}, {10, 9, 8}};
-        int[][] array1 = new int[8][8];
-        int[][] array2 = new int[][]{{10, 9, 8}, {8, 6, 5}, {4, 3, 2}};
-        Assertions.assertFalse(Task2.checker(array));
-        Assertions.assertTrue(Task2.checker(array1));
-        Assertions.assertTrue(Task2.checker(array2));
+        Assertions.assertFalse(Task2.checker(new int[][]{{10, 402, 3}, {123, 120, 110}, {10, 9, 8}}));
+    }
+    @Test
+    void checker_Sort_True() {
+        Assertions.assertTrue(Task2.checker(new int[8][8]));
+        Assertions.assertTrue(Task2.checker(new int[][]{{10, 9, 8}, {8, 6, 5}, {4, 3, 2}}));
     }
 }
